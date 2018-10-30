558461cf9506bec5428c92ca922e431ea86d1744
Jackkidan <eddie59@mail.ru>
2018-10-30 20:32:06 +0300

    commit2

--- src/test/java/OzonAddItemsTest.java
+++ src/test/java/OzonAddItemsTest.java
@@ -1,26 +1,31 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import pages.Init;
 import pages.MainPage;
 import pages.SearchResultPage;
+import pages.ShoppingCartPage;
 
 public class OzonAddItemsTest {
+
     MainPage mainPage = new MainPage();
     SearchResultPage searchResultPage = new SearchResultPage();
+    ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
 
     @Before
     public void setUp() {
-        mainPage.setUp();
+        Init.setUp();
     }
 
     @After
-    public void tearDown() {
-       // mainPage.tearDown();
-    }
+    public void tearDown() { Init.tearDown(); }
 
     @Test
     public void checkCorrectAddToCart() {
         mainPage.findInSearch("iPhone 7 Plus/8 Plus Black");
         searchResultPage.findElementsInPage();
+        searchResultPage.clickViewCart();
+        shoppingCartPage.checkTotalPrice();
+        shoppingCartPage.checkTotatQuantity(5);
     }
 }
